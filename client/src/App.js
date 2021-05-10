18fc63780ee9d6c8d225231577fa11551b35c3ab
Matusie214 <matusiakcezary@wp.pl>
2021-05-10 11:49:45 +0200

    stworzenie polaczenia z baza (wysylanie i odbieranie danych z bazy)

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
+import React,{useState,useEffect} from 'react'
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  const[movieName,setMovieName]=useState('')
+  const[review,setReview]=useState('')
+  useEffect(()=>{
+    Axios.get('http://localhost:3001/api/get').then((response)=>{
+      console.log(response)
+    });
+  },[]);
+  
+  const submitReview = ()=>{
+    Axios.post('http://localhost:3001/api/insert',{
+      movieName:movieName,movieReview:review}).then(()=>{
+      alert("sucess")})
+  }
   return (
     <div className="App">
       <h1>CRUD APP</h1>
 
       <div className="form">
         <label>Movie name:</label>
-        <input type="text" name="movieName"/>
+        <input type="text" name="movieName" onChange={(e)=>
+        setMovieName(e.target.value)}/>
         <label>Movie review:</label>
-        <input type="text" name="review"/>
-        <button>Submit</button>
+        <input type="text" name="review" onChange={(e)=>
+        setReview(e.target.value)}/>
+        <button onClick={submitReview()}>Submit</button>
       </div>
 
     </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express =require("express")
+const bodyParser= require('body-parser')
 const app= express()
 const mysql= require("mysql")
+const cors=require('cors')
 
 const db =mysql.createPool({
     host:"localhost",
@@ -11,13 +13,36 @@ const db =mysql.createPool({
 
 
 
-app.get('/',(req,res)=>{
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/api/get',(req,res)=>{
+    
+    const sqlSelect="SELECT * FROM `movies_reviews` ;"
+    db.query(sqlSelect, (err,result)=>{
+        console.log(result)
+
+    })
+})
+app.post('/api/insert',(req,res)=>{
+    const moveName=req.body.movieName
+    const movieReview=req.body.movieReview
+    const sqlInsert=
+    "INSERT INTO `movies_reviews` (`ID`, `movie`, `review`) VALUES (NULL, ?, ?);"
+    db.query(sqlInsert,[moveName,movieReview], (err,result)=>{
+        console.log(result)
+
+    })
+})
+
+//app.get('/',(req,res)=>{
    // const sqlInsert= "INSERT INTO `movies_reviews` (`ID`, `movie`, `review`) VALUES (NULL, 'siema', 'test2');"
    // db.query(sqlInsert,(err,result)=>{
    //     res.send("Hello world2")
 
 //    })
-})
+//})
 
 
 app.listen(3001,()=>{
